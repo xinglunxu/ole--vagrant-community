a2de178f373eb7ca3f06eb20dafbe1c2006722a9
xinglunxu <xinglunxu@gmail.com>
2016-06-29 16:46:32 -0700

    Add detectfirewall.sh

--- macosx/detectfirewall.sh
+++ macosx/detectfirewall.sh
@@ -1,5 +1,16 @@
 #!/bin/bash   
 
-IP=ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2
+COMMAND="ifconfig | grep \"inet \" | grep -v 127.0.0.1 | cut -d' '  -f2"
+INET_IP=`eval $COMMAND`
+PORT=$1
+if [ "$PORT" = "" ] ; then
+	PORT=5984
+fi
 
-echo $IP
+HTTP_RES_CODE=`curl -Is http://${INET_IP}:${PORT} | head -1 | cut -d' '  -f2`
+
+if [ "$HTTP_RES_CODE" = "200" ] ; then
+	echo "off"
+else 
+	echo "on"
+fi
